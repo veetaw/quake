6de358de31684b3c0cbc9d220874e7f5b30b3569
vito <13486742+veetaw@users.noreply.github.com>
2018-12-24 15:25:12 +0100

    travis: python 3.6

--- scripts/format_files.py
+++ scripts/format_files.py
@@ -5,7 +5,7 @@ import os
 
 BRANCH = "travis-"+os.getenv("TRAVIS_BUILD_NUMBER", "unknown")
 subprocess.call("git checkout -b " + BRANCH, shell=True)
-subprocess.call("../../flutter/bin/flutter format ../lib/ ../test/", shell=True)
+subprocess.call("../flutter/bin/flutter format lib/ test/", shell=True)
 subprocess.call("git config --global user.email \"travis@travis-ci.org\"", shell=True)
 subprocess.call("git config --global user.name \"Travis CI\"", shell=True)
 
@@ -17,7 +17,7 @@ if len(changes.stdout.decode()) > 0:
     commit_message += changes.stdout.decode()
 
     subprocess.call("git add -A", shell=True)
-    subprocess.call("git commit --message " + commit_message, shell=True)
+    subprocess.call("git commit --message \"" + commit_message + "\"", shell=True)
 
     subprocess.call("git remote add travis https://" + os.getenv("GITHUB_TOKEN") + "@github.com/veetaw/quake.git", shell=True)
     subprocess.call("git push --quiet travis " + BRANCH, shell=True)
