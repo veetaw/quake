ebca945c50914870507d7bc0c01ab27829f67f9e
Travis CI <travis@travis-ci.org>
2018-12-24 14:37:46 +0000

    TRAVIS BUILD: 24 formatted files: test/earthquake_provider_test.dart

--- test/earthquake_provider_test.dart
+++ test/earthquake_provider_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:quake/src/db/earthquake_provider.dart';
 
-
 /// TODO tests
 void main() {
   // EarthquakeProvider earthquakeProvider = EarthquakeProvider();
