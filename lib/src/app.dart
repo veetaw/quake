3c6a97ae2bc68a95fe95d7ee479732f5449fc115
vito <13486742+veetaw@users.noreply.github.com>
2018-12-30 16:17:44 +0100

    basic implementation of home ui

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quake/src/locale/localizations.dart';
 
 class Home extends StatefulWidget {
   static const routeName = "/home";
@@ -9,11 +10,49 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  static const double _kAppBarElevation = 2.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).bottomAppBarColor,
+        brightness: Theme.of(context).brightness, // make status bar icons dark or light depending on the brightness
+        centerTitle: Theme.of(context).platform ==
+            TargetPlatform.iOS, // center title if running on ios
+        primary: true,
+        iconTheme: Theme.of(context).iconTheme,
+        textTheme: Theme.of(context).textTheme,
+        title: Text(QuakeLocalizations.of(context).title),
+        elevation: _kAppBarElevation,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          _buildBottomNavigationBarItem(icon: Icons.chrome_reader_mode, text: ""),
+          _buildBottomNavigationBarItem(icon: Icons.location_on, text: ""),
+          _buildBottomNavigationBarItem(icon: Icons.map, text: ""),
+        ],
+        currentIndex: 0, // TODO, bloc
+      ),
     );
+  }
+
+  BottomNavigationBarItem _buildBottomNavigationBarItem({@required IconData icon, @required String text}) {
+    return BottomNavigationBarItem(
+          icon: Icon(icon),
+          title: Text(text),
+        );
   }
 
   @override
