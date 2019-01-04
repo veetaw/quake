a05762908833902f96991ab43d9e831dd15b9595
vito <13486742+veetaw@users.noreply.github.com>
2019-01-04 16:54:41 +0100

    earthquake card bottom info tile

--- lib/src/model/earthquake_card.dart
+++ lib/src/model/earthquake_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:quake/src/locale/localizations.dart';
 import 'package:quake/src/model/earthquake.dart';
 
 class EarthquakeCard extends StatelessWidget {
@@ -15,10 +17,12 @@ class EarthquakeCard extends StatelessWidget {
   final ShapeBorder _kCardShape = RoundedRectangleBorder(
     borderRadius: BorderRadius.circular(8.0),
   );
-  static const EdgeInsets _kMarginBetween = EdgeInsets.all(9.0);
+  static const EdgeInsets _kCardMargin = EdgeInsets.all(9.0);
   static const double _kEarthquakeLocationNameSize = 20;
   static const double _kEarthquakeDateSize = 16;
   static const double _kEarthquakeHourSize = 14;
+  static const double _kPaddingBetweenText = 9;
+  static const double _kPaddingTopBottomInfos = 2;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +33,7 @@ class EarthquakeCard extends StatelessWidget {
         color: Theme.of(context).cardColor,
         elevation: _kCardElevation,
         shape: _kCardShape,
-        margin: _kMarginBetween,
+        margin: _kCardMargin,
         child: InkWell(
           onTap: onTap,
           child: Column(
@@ -40,18 +44,39 @@ class EarthquakeCard extends StatelessWidget {
                 text: earthquake.eventLocationName,
                 size: _kEarthquakeLocationNameSize,
               ),
-              _buildText(
-                context: context,
-                text: earthquake.eventLocationName,
-                size: _kEarthquakeDateSize,
-                weight: FontWeight.w300,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: _kPaddingBetweenText,
+                  vertical: _kPaddingBetweenText / 2,
+                ),
               ),
               _buildText(
                 context: context,
-                text: earthquake.eventLocationName,
+                text: earthquake.time.toString(), // TODO: date localization
+                size: _kEarthquakeDateSize,
+                weight: FontWeight.w300,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: _kPaddingBetweenText,
+                  vertical: _kPaddingBetweenText / 2,
+                ),
+              ),
+              _buildText(
+                context: context,
+                text: DateFormat.Hm(QuakeLocalizations.localeCode)
+                    .format(earthquake.time),
                 size: _kEarthquakeHourSize,
                 weight: FontWeight.w200,
-              )
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: _kPaddingTopBottomInfos,
+                ),
+              ),
+              _EarthquakeCardBottomInfos(
+                earthquake: earthquake,
+              ),
             ],
           ),
         ),
@@ -76,4 +101,67 @@ class EarthquakeCard extends StatelessWidget {
           color: Theme.of(context).textTheme.title.color,
         ),
       );
+}
+
+class _EarthquakeCardBottomInfos extends StatelessWidget {
+  final Earthquake earthquake;
+
+  _EarthquakeCardBottomInfos({
+    @required this.earthquake,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _EarthquakeCardBottomTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const _EarthquakeCardBottomTile({
+    @required this.title,
+    @required this.subtitle,
+    @required this.icon,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: ListTile(
+          leading: Icon(
+            icon,
+            color: Theme.of(context).iconTheme.color,
+          ),
+          title: Column(
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                  // fontFamily: 'Roboto', TODO
+                  fontStyle: FontStyle.normal,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryTextTheme.title.color,
+                ),
+              ),
+              Text(
+                subtitle.toUpperCase(),
+                style: TextStyle(
+                  // fontFamily: 'Roboto', TODO
+                  fontStyle: FontStyle.normal,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                  color: Theme.of(context).primaryTextTheme.title.color,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
