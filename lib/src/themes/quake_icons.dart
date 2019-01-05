78359842440010d8d7140c02c06213a1f94b6d49
vito <13486742+veetaw@users.noreply.github.com>
2019-01-05 10:14:03 +0100

    fix typo

--- lib/src/themes/quake_icons.dart
+++ lib/src/themes/quake_icons.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 class QuakeIcons {
   QuakeIcons._();
 
-  static const _kFontFam = 'QuakeIcons';
+  static const _kFontFam = 'Quake';
 
   static const IconData pulse = const IconData(0xe800, fontFamily: _kFontFam);
   static const IconData earth = const IconData(0xe801, fontFamily: _kFontFam);
