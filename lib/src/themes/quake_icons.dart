fbe80f362b4d32ede8860c80881b2a43250bcc6e
vito <13486742+veetaw@users.noreply.github.com>
2019-01-05 08:15:41 +0100

    added custom icon font

---
+++ lib/src/themes/quake_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class QuakeIcons {
+  QuakeIcons._();
+
+  static const _kFontFam = 'QuakeIcons';
+
+  static const IconData pulse = const IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData earth = const IconData(0xe801, fontFamily: _kFontFam);
+}
