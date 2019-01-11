6a73e230d168f22dd6ce3c67fe879e4a3a36e4a5
vito <13486742+veetaw@users.noreply.github.com>
2019-01-11 11:36:19 +0100

    basic implementation of home screen

--- lib/src/model/homepage_all.dart
+++ lib/src/model/homepage_all.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quake/src/bloc/earthquakes_bloc.dart';
+import 'package:quake/src/model/earthquake_card.dart';
+
+final EarthquakesBloc earthquakesBloc = EarthquakesBloc();
 
 class HomePageAll extends StatelessWidget {
   static HomePageAll _instance = HomePageAll._();
@@ -7,8 +11,28 @@ class HomePageAll extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    earthquakesBloc.fetchData();
+
     return Container(
-      child: Text(this.hashCode.toString()),
+      child: StreamBuilder(
+        stream: earthquakesBloc.earthquakes,
+        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+          if (snapshot.hasError)
+            return Container(
+              child: Text("error"),
+            ); // TODO:
+          else if (snapshot.hasData)
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (BuildContext context, int index) => EarthquakeCard(
+                    earthquake: snapshot.data[index],
+                    onTap: () {},
+                  ),
+            );
+          else
+            return CircularProgressIndicator(); // TODO:
+        },
+      ),
     );
   }
 }
