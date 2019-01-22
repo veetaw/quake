0aeef1a1f6ec7f7647d8788780f8b5531654c01d
vito <13486742+veetaw@users.noreply.github.com>
2019-01-22 18:17:59 +0100

    updated iconfont

--- lib/src/themes/quake_icons.dart
+++ lib/src/themes/quake_icons.dart
@@ -7,4 +7,5 @@ class QuakeIcons {
 
   static const IconData pulse = const IconData(0xe800, fontFamily: _kFontFam);
   static const IconData earth = const IconData(0xe801, fontFamily: _kFontFam);
+  static const IconData location = const IconData(0xe802, fontFamily: _kFontFam);
 }
