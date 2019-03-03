22de83112bed093b8c675a63acda54b04b13a27d
vito <13486742+veetaw@users.noreply.github.com>
2019-03-03 17:43:37 +0100

    earthquake details - location marker on map - implemented left infos - mocked right infos

--- lib/src/model/earthquake_details.dart
+++ lib/src/model/earthquake_details.dart
@@ -3,8 +3,8 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 import 'package:quake/src/locale/localizations.dart';
 import 'package:quake/src/model/earthquake.dart';
-import 'package:quake/src/model/vertical_divider.dart'
-    as vd; // to ignore ambiguos import
+import 'package:quake/src/model/vertical_divider.dart' as vd;
+import 'package:quake/src/themes/quake_icons.dart'; // to ignore ambiguos import
 
 class EarthquakeDetails extends StatelessWidget {
   final Earthquake earthquake;
@@ -13,6 +13,8 @@ class EarthquakeDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double paddingBetween = 10;
+
     return Scaffold(
       appBar: _buildAppBar(context),
       body: ConstrainedBox(
@@ -27,36 +29,67 @@ class EarthquakeDetails extends StatelessWidget {
                   center: LatLng(earthquake.latitude, earthquake.longitude),
                   zoom: 8.0,
                   interactive: false,
-                  ),
+                ),
                 layers: [
                   TileLayerOptions(
                     urlTemplate:
                         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                     subdomains: ['a', 'b', 'c'],
                   ),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                        point:
+                            LatLng(earthquake.latitude, earthquake.longitude),
+                        builder: (BuildContext context) {
+                          // TODO: use an animation instead of the marker
+                          // FlareActor(
+                          //   "assets/flare/pulsing circle.flr",
+                          //   animation: "Untitled",
+                          // );
+                          return Icon(
+                            QuakeIcons.location,
+                            color: Theme.of(context).errorColor,
+                            size: 45,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ],
-              ), // FlutterMap
+              ),
             ),
             Expanded(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Expanded(
-                    // left infos
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        // tile
-                        // padding
-                        // tile
-                        // padding
-                        // tile
+                        _buildLeftTile(
+                            context,
+                            paddingBetween,
+                            earthquake.magnitude.toString(),
+                            QuakeLocalizations.of(context).magnitude),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: paddingBetween,
+                          ),
+                        ),
+                        _buildLeftTile(
+                          context,
+                          paddingBetween,
+                          earthquake.depth.toString(),
+                          QuakeLocalizations.of(context).depth,
+                        )
                       ],
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 16.0),
                     child: vd.VerticalDivider(
                       // divider
                       width: 2,
@@ -72,9 +105,46 @@ class EarthquakeDetails extends StatelessWidget {
                       children: <Widget>[
                         Column(
                           children: <Widget>[
-                            ListTile(), // date
-                            ListTile(), // location
-                            ListTile(), // people involved
+                            ////////////////////////////////////////////////////////////
+                            ///// TODO:          TEST                             //////
+                            ////////////////////////////////////////////////////////////
+                            SizedBox(
+                              width: double.infinity,
+                              height:
+                                  (MediaQuery.of(context).size.height / 2) / 4 -
+                                      paddingBetween,
+                              child: Container(
+                                color: Colors.red,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: paddingBetween,
+                              ),
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              height:
+                                  (MediaQuery.of(context).size.height / 2) / 4 -
+                                      paddingBetween,
+                              child: Container(
+                                color: Colors.red,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: paddingBetween,
+                              ),
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              height:
+                                  (MediaQuery.of(context).size.height / 2) / 4 -
+                                      paddingBetween,
+                              child: Container(
+                                color: Colors.red,
+                              ),
+                            ),
                           ],
                         ),
                       ],
@@ -88,6 +158,36 @@ class EarthquakeDetails extends StatelessWidget {
       ),
     );
   }
+
+  SizedBox _buildLeftTile(
+    BuildContext context,
+    num paddingBetween,
+    String title,
+    String description,
+  ) =>
+      SizedBox(
+        width: double.infinity,
+        height: (MediaQuery.of(context).size.height / 2) / 3 - paddingBetween,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 60,
+              ),
+            ),
+            Text(
+              description,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ],
+        ),
+      );
 
   AppBar _buildAppBar(BuildContext context) {
     return AppBar(
