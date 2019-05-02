1f1f7ddd920c77185782f22e6587a46f529301e8
Vito <13486742+veetaw@users.noreply.github.com>
2019-05-02 19:49:43 +0200

    round distances to two decimal numbers

--- lib/src/utils/unit_of_measurement_conversion.dart
+++ lib/src/utils/unit_of_measurement_conversion.dart
@@ -12,16 +12,18 @@ class UnitOfMeasurementConversion {
   }) {
     switch (unit) {
       case UnitOfMeasurement.meters:
-        return kmValue * 1000;
+        return _round(kmValue * 1000);
       case UnitOfMeasurement.miles:
-        return kmValue / 1.609;
+        return _round(kmValue / 1.609);
       default:
-        return kmValue;
+        return _round(kmValue);
     }
   }
 
   static UnitOfMeasurement unitOfMeasurementFromString(String s) =>
       UnitOfMeasurement.values.firstWhere((u) => u.toString() == s);
 }
+
+double _round(double value) => (value  * 10e1).truncate() / 10e1;
 
 enum UnitOfMeasurement { kilometers, meters, miles }
