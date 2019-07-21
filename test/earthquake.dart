c5a4ac919c58b659d6add08d374a58a71860e12f
vito <vitopiegari01@gmail.com>
2019-07-21 18:07:51 +0200

    Adds test for Earthquake class

---
+++ test/earthquake.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:quake/src/api/model/earthquake.dart';
+
+import 'package:test/test.dart';
+
+main() {
+  group('Earthquake', () {
+    Map _jsonText;
+
+    setUp(() async {
+      _jsonText =
+          json.decode(await File('test/earthquake.json').readAsString());
+    });
+
+    test('fromJson constructor should work correctly with valid data', () {
+      Earthquake _earthquake = Earthquake.fromJson(_jsonText);
+
+      expect(_earthquake, isNotNull);
+      expect(_earthquake.type, isNotNull);
+      expect(_earthquake.eventName, isNotNull);
+
+      expect(_earthquake.coordinates.latitude, isNotNull);
+      expect(_earthquake.coordinates.longitude, isNotNull);
+    });
+
+    test('fromJson should handle nullable fields', () {
+      _jsonText["origin"]["depth"]["uncertainity"] = null;
+      _jsonText["magnitude"]["mag"]["uncertainity"] = null;
+      _jsonText["magnitude"]["stationCount"] = null;
+
+      Earthquake _earthquake = Earthquake.fromJson(_jsonText);
+
+      expect(_earthquake.depthUncertainity, isZero);
+      expect(_earthquake.magnitudeUncertainity, isZero);
+      expect(_earthquake.stationCount, isZero);
+    });
+  });
+}
