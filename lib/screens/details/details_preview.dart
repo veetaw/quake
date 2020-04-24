dacff713eaf950f699fe517837f694b1d7c67df5
vito <vitopiegari01@gmail.com>
2020-04-24 09:41:39 +0200

    WIP: Initial implementation of preview dialog

--- lib/screens/details/details_preview.dart
+++ lib/screens/details/details_preview.dart
@@ -1,6 +1,10 @@
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+
 import 'package:quake/models/earthquake.dart';
 
 class DetailsPreview extends StatelessWidget {
@@ -20,18 +24,71 @@ class DetailsPreview extends StatelessWidget {
 
     final bool isPortrait = mediaQuery.orientation == Orientation.portrait;
 
-    return Hero(
-      tag: earthquake,
-      child: BackdropFilter(
-        filter: ui.ImageFilter.blur(
-          sigmaX: 8.0,
-          sigmaY: 8.0,
+    final double dialogHeight = height / 2;
+    final double dialogWidth = width - 64;
+
+    // todo: add hero for animation
+    return BackdropFilter(
+      filter: ui.ImageFilter.blur(
+        sigmaX: 8.0,
+        sigmaY: 8.0,
+      ),
+      child: Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
         ),
-        child: Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+        child: Container(
+          height: dialogHeight,
+          width: dialogWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: dialogHeight / 2,
+                width: dialogWidth,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(16),
+                  ),
+                  child: FlutterMap(
+                    options: MapOptions(
+                      center: earthquake.coordinates.asLatLng(),
+                      zoom: 8.0,
+                      interactive: false,
+                    ),
+                    layers: <LayerOptions>[
+                      TileLayerOptions(
+                        urlTemplate:
+                            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a', 'b', 'c'],
+                      ),
+                      MarkerLayerOptions(
+                        markers: <Marker>[
+                          Marker(
+                            point: earthquake.coordinates.asLatLng(),
+                            builder: (context) =>
+                                ColoredBox(color: Colors.red), // TODO
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              RaisedButton(
+                child: Container(
+                  width: dialogWidth / 1.5,
+                  alignment: Alignment.center,
+                  child: Text("Full details"),
+                ),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).cardColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                onPressed: () {},
+              ),
+            ],
           ),
-          child: Container(),
         ),
       ),
     );
