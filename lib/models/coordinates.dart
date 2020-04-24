78fe91670884bf216161057d6416ca5fdf27a838
vito <vitopiegari01@gmail.com>
2020-04-24 09:41:15 +0200

    Add asLatLng to Coordinates class

--- lib/models/coordinates.dart
+++ lib/models/coordinates.dart
@@ -1,6 +1,10 @@
+import 'package:latlong/latlong.dart';
+
 class Coordinates {
   final double latitude;
   final double longitude;
 
   const Coordinates(this.latitude, this.longitude);
+
+  LatLng asLatLng() => LatLng(latitude ?? 0, longitude ?? 0);
 }
